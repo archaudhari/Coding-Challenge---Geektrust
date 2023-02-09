b0ad56e9b3358ec3f74522f197a01acb43b4861b
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-02-09 13:55:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const trainA = ["ENGINE", "NDL", "GHY", "NJP", "NGP"];
-const trainB = ["ENGINE", "NJP", "GHY", "AGA", "BPL", "PTAP", "NDL"];
+// Context
+//  There are 2 super fast trains, Train A and Train B. Train A travels from Chennai to New Delhi. Train B travels from Trivandrum to Guwahati.
+
+//  Passengers can board these trains only at the source station.
+//  The trains have only reserved bogies and each bogie will only have passengers to a specific station.
+//  When the train arrives at a station, the entire bogie with passengers is detached from the train, and the train continues its journey.
+//  The routes with station code and distances of each station from originating station are as follows: (STATION (CODE) - DISTANCE ):
+
+const trainA = ['ENGINE', 'NDL', 'GHY', 'NJP', 'NGP'];
+const trainB = ['ENGINE', 'NJP', 'GHY', 'AGA', 'BPL', 'PTAP', 'NDL'];
 
 const mergeTrains = (trainA, trainB) => {
   const trainAHyd = [];
@@ -9,12 +17,12 @@ const mergeTrains = (trainA, trainB) => {
   let i = 0,
     j = 0;
 
-  while (i < trainA.length && trainA[i] !== "GHY") {
+  while (i < trainA.length && trainA[i] !== 'GHY') {
     trainAHyd.push(trainA[i]);
     i++;
   }
 
-  while (j < trainB.length && trainB[j] !== "GHY") {
+  while (j < trainB.length && trainB[j] !== 'GHY') {
     trainBHyd.push(trainB[j]);
     j++;
   }
@@ -56,13 +64,13 @@ const mergeTrains = (trainA, trainB) => {
   };
 };
 
-console.log("ARRIVAL");
-console.log("TRAIN A", trainA);
-console.log("TRAIN B", trainB);
+console.log('ARRIVAL');
+console.log('TRAIN A', trainA);
+console.log('TRAIN B', trainB);
 
 const result = mergeTrains(trainA, trainB);
 
-console.log("DEPARTURE");
-console.log("TRAIN A", result.trainAHyd);
-console.log("TRAIN B", result.trainBHyd);
-console.log("TRAIN AB", result.trainABDep);
+console.log('DEPARTURE');
+console.log('TRAIN A', result.trainAHyd);
+console.log('TRAIN B', result.trainBHyd);
+console.log('TRAIN AB', result.trainABDep);
